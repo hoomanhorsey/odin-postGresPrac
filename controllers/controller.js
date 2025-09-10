b61a32b1ae75bb628ec076f639849568b9fd3ad8
Hermshy Mac <94696321+hoomanhorsey@users.noreply.github.com>
2025-09-10 13:53:13 +1000

    Added url parsing via req.query object

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,7 @@
 const db = require("../db/queries");
 
 async function getUsernames(req, res) {
+  console.log("req query is", req.query.search);
   const usernames = await db.getAllUsernames();
   console.log("Usernames: ", usernames);
   res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 
+require("dotenv").config();
+
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "hermshy",
-  database: "top_users",
-  password: "",
-  port: 5432, // The default port
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT, // The default port
 });
